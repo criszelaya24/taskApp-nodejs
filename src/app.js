45587cf93d9f4275004bd2d1b7a088d398b4f1db
cristopher Palacios <cristopher@fanatiz.com>
2020-08-31 13:50:27 -0400

    exporting routes to individual files

--- src/app.js
+++ src/app.js
@@ -2,41 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const sendError = require('./utils/sendError');
-const User = require('./models/User'),
-    Task = require('./models/Task');
+const usersRouter = require('./routes/user'),
+    taskRouter = require('./routes/task');
 
 app.use(express.json());
 
 app.get('/', (req, res) => res.send({ hello: 'Hello World!' }));
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
+app.use(usersRouter);
 
-    try {
-        await user.save();
-
-        res.send(201, user);
-    } catch (error) {
-        sendError(res, error);
-    }
-});
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body);
-
-    try {
-        await task.save();
-
-        res.send((201, task));
-    } catch (error) {
-        sendError(res, error);
-    }
-});
+app.use(taskRouter);
 
 app.all('*', (req, res) => {
-    sendError(res, {
-        error: 'Endpoint not found',
-    });
+    sendError(res, 'Endpoint not found');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose;
